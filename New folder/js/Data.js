aa9895e4ec94aaf301ef5ba0e01b4cd3214c5cc2
tranthiendat217 <99645584+tranthiendat217@users.noreply.github.com>
2022-04-22 23:46:43 +0700

    Add files via upload

---
+++ New folder/js/Data.js
@@ -0,0 +1,89 @@
+
+
+
+let sp1 = {masp:1,hinhanh:'../image/SamSung/Samsung-Galaxy-A52s-5G1.jpg', tensp:"Samsung Galaxy A52s 5G", soluong:1, dongia:8990000, description:"White Violet Black Green"}
+let sp2 = {masp:2,hinhanh:'../image/SamSung/Samsung-Galaxy-S21-FE.jpg', tensp:"Samsung Galaxy S21 FE 5G 8/256GB", soluong:1, dongia:15990000, description:"White Graphite Black Green"}
+let sp3 = {masp:3,hinhanh:'../image/SamSung/Samsung-Galaxy-S21Plus.jpg', tensp:"Samsung Galaxy S21+ 5G 8/128GB", soluong:1, dongia:17990000, description:"Violet Black Silver"}
+let sp4 = {masp:4,hinhanh:'../image/SamSung/Samsung-Galaxy-Z-Fold-2-DB.jpg', tensp:"Samsung Galaxy Z Fold 2 5G Phiên bản mùa hè ", soluong:1, dongia:33000000, description:"Black Bronze"}
+let sp5 = {masp:5,hinhanh:'../image/SamSung/Samsung-Galaxy-S22-Plus.jpg', tensp:"Samsung Galaxy S22 Plus 8/128GB", soluong:1, dongia:25990000, description:"White Pink Black Green"}
+let sp6 = {masp:6,hinhanh:'../image/SamSung/Samsung-Galaxy-S22.jpg', tensp:"Samsung Galaxy S22 8/128GB", soluong:1, dongia:21990000, description:"White Pink Black Green"}
+let sp7 = {masp:7,hinhanh:'../image/SamSung/Samsung-Galaxy-S22-Ultra-gre-1.jpg', tensp:"Samsung Galaxy S22 Ultra 12/256GB", soluong:1, dongia:33990000, description:"Red White Black Green"}
+let sp8 = {masp:8,hinhanh:'../image/SamSung/Samsung-Galaxy-Z-Flip3-5G-Cream-1.jpg', tensp:"Samsung Galaxy Z Flip3 5G 128GB", soluong:1, dongia:20490000, description:"Purple Black Green BieGe"}
+let sp9 = {masp:9,hinhanh:'../image/SamSung/Samsung-Galaxy-A22-grn-1.jpg', tensp:"Samsung Galaxy A22", soluong:1, dongia:5890000, description:"Violet Black Green"}
+let sp10 = {masp:10,hinhanh:'../image/SamSung/Samsung-Galaxy-A52s-5G1.jpg', tensp:"Samsung Galaxy Z Fold3 5G 256GB", soluong:1, dongia:34990000, description:"Black Silver Green"}
+
+let dssp_samsung = [sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10]
+localStorage.setItem("products_samsung", JSON.stringify(dssp_samsung))
+
+let sp11 = {masp:11,hinhanh:'../image/Oppo/Oppo-A16-Sil.jpg', tensp:"OPPO A16", soluong:1, dongia:3990000, description:"Black Silver"}
+let sp12 = {masp:12,hinhanh:'../image/Oppo/OPPO-Reno7 5G-ble.jpg', tensp:"OPPO Reno7 5G", soluong:1, dongia:12990000, description:"Blue Black"}
+let sp13 = {masp:13,hinhanh:'../image/Oppo/OPPO-A94-Pur-.jpg', tensp:"OPPO A94", soluong:1, dongia:5990000, description:"Purple Black"}
+let sp14 = {masp:14,hinhanh:'../image/Oppo/OPPO-Reno6-Z-ble-1.jpg', tensp:"OPPO Reno6 Z 5G", soluong:1, dongia:7990000, description:"Black Silver"}
+let sp15 = {masp:15,hinhanh:'../image/Oppo/OPPO-A55-grn-1.jpg', tensp:"OPPO A55", soluong:1, dongia:4990000, description:"Blue Black Green"}
+let sp16 = {masp:16,hinhanh:'../image/Oppo/OPPO-A73-Oran-1.jpg', tensp:"OPPO A73", soluong:1, dongia:5490000, description:"Orange Black Silver"}
+let sp17 = {masp:17,hinhanh:'../image/Oppo/OPPO-A15s-Blu-1.jpg', tensp:"OPPO A15s", soluong:1, dongia:3090000, description:"Black Silver"}
+let sp18 = {masp:18,hinhanh:'../image/Oppo/OPPO-A76-ble-1-1.jpg', tensp:"OPPO A76", soluong:1, dongia:5990000, description:"Blue Black"}
+let sp19 = {masp:19,hinhanh:'../image/Oppo/OPPO-A95-sli-1.jpg', tensp:"OPPO A95", soluong:1, dongia:6990000, description:"Black Silver"}
+
+
+let dssp_op = [sp11,sp12,sp13,sp14,sp15,sp16,sp17,sp18,sp19]
+localStorage.setItem("products_op", JSON.stringify(dssp_op))
+
+
+let sp20 = {masp:20,hinhanh:'../image/Iphone/Iphone11.jpg', tensp:"IPhone 11 128GB", soluong:1, dongia:14990000, description:"White Black Green"}
+let sp21 = {masp:21,hinhanh:'../image/Iphone/Iphone12.jpg', tensp:"IPhone 12 64GB", soluong:1, dongia:16690000, description:"Red White Blue Black Green"}
+let sp22 = {masp:22,hinhanh:'../image/Iphone/IPhone-12-128GB-Mini-Red-1.jpg', tensp:"iPhone 12 128GB", soluong:1, dongia:18490000, description:"Blue Black"}
+let sp23 = {masp:23,hinhanh:'../image/Iphone/IPhone 11-Red-64GB.jpg', tensp:"iPhone 11 64GB", soluong:1, dongia:12990000, description:"Red White Black Green"}
+let sp24 = {masp:24,hinhanh:'../image/Iphone/iPhone-12-Mini-Bla-128GB-1.jpg', tensp:"iPhone 12 Mini 128GB", soluong:1, dongia:14490000, description:"White Blue Black Green"}
+let sp25 = {masp:25,hinhanh:'../image/Iphone/Iphone13ProMaxBle.jpg', tensp:"IPhone 13 Pro Max 128GB", soluong:1, dongia:30490000, description:"Graphite Sierra AlPine-Green Gold Silver"}
+let sp26 = {masp:26,hinhanh:'../image/Iphone/Iphone13ProMaxSir.jpg', tensp:"IPhone 13 Pro Max 1TB", soluong:1, dongia:42990000, description:"Graphite Sierra AlPine-Green Gold Silver"}
+let sp27 = {masp:27,hinhanh:'../image/Iphone/iPhone-13-Pro-Max-512GB-ghte-1.jpg', tensp:"iPhone 13 Pro 512GB", soluong:1, dongia:34190000, description:"Graphite Sierra-Blue AlPine-Green Gold Silver"}
+let sp28 = {masp:28,hinhanh:'../image/Iphone/1iPhone-13-mini-128GB-pik-1.jpg', tensp:"iPhone 13 mini 128GB", soluong:1, dongia:18990000, description:"Red Blue Pink StarLight MidNight Green"}
+let sp29 = {masp:29,hinhanh:'../image/Iphone/1iPhone-13-Pro-Max-256GB-gld-1.jpg', tensp:"IPhone 13 Pro 256GB", soluong:1, dongia:29990000, description:"Graphite Sierra-Blue AlPine Green Gold Silver"}
+let sp30 = {masp:30,hinhanh:'../image/Iphone/iPhone 12 Pro -Pro Max-512GB.jpg', tensp:"iPhone 12 Pro Max 512GB", soluong:1, dongia:39990000, description:"Graphite Pacific-Blue Gold Silver"}
+
+
+let dssp_ip = [sp20,sp21,sp22,sp23,sp24,sp25,sp26,sp27,sp28,sp29,sp30]
+localStorage.setItem("products_ip", JSON.stringify(dssp_ip))
+
+
+let sp31 = {masp:31,hinhanh:'../image/Realme/Realme-C21Y.jpg', tensp:"Realme C21Y (4-64GB)", soluong:1, dongia:3290000, description:"Blue Black"}
+let sp32 = {masp:32,hinhanh:'../image/Realme/realme-5i-green.jpg', tensp:"Realme 5i 3GB-32GB", soluong:1, dongia:1990000, description:"Blue Green"}
+let sp33 = {masp:33,hinhanh:'../image/Realme/realme-6pro.png', tensp:"Realme 6 Pro", soluong:1, dongia:7990000, description:"Blue Red"}
+let sp34 = {masp:34,hinhanh:'../image/Realme/realme-8-Pro-Blu.jpg', tensp:"Realme 8 Pro", soluong:1, dongia:7250000, description:"Blue Black"}
+let sp35 = {masp:35,hinhanh:'../image/Realme/realme-C25Y-ble-1.jpg', tensp:"Realme C25Y", soluong:1, dongia:3990000, description:"Blue Grey"}
+let sp36 = {masp:36,hinhanh:'../image/Realme/realme-9i-ble-1.jpg', tensp:"Realme 9i ", soluong:1, dongia:5990000, description:"Blue Black"}
+let sp37 = {masp:37,hinhanh:'../image/Realme/Realme-C11-2021-(4GB-64GB)-gry-1.jpg', tensp:"Realme C11 2021 (4GB - 64GB)", soluong:1, dongia:3490000, description:"Blue Grey"}
+let sp38 = {masp:38,hinhanh:'../image/Realme/Realme-C25-Grey-1.jpg', tensp:"Realme C25", soluong:1, dongia:3990000, description:"Blue Black"}
+let sp39 = {masp:39,hinhanh:'../image/Realme/Realme-5s.jpg', tensp:"Realme 5s", soluong:1, dongia:4990000, description:"Blue Black Red"}
+let sp40 = {masp:40,hinhanh:'../image/Realme/Realme-XT.png', tensp:"Realme XT", soluong:1, dongia:7990000, description:"white Blue Black"}
+
+
+let dssp_rm = [sp31,sp32,sp33,sp34,sp35,sp36,sp37,sp38,sp39,sp40]
+localStorage.setItem("products_rm", JSON.stringify(dssp_rm))
+
+let sp41 = {masp:41,hinhanh:'../image/Vivo/Vivo-V23e.jpg', tensp:"Vivo V23e", soluong:1, dongia:7490000, description:"Blue Black"}
+let sp42 = {masp:42,hinhanh:'../image/Vivo/vivo-V23-5G.jpg', tensp:"Vivo V23 5G", soluong:1, dongia:12490000, description:"Gold Black"}
+let sp43 = {masp:43,hinhanh:'../image/Vivo/Vivo-Y21s.jpg', tensp:"Vivo Y21s 4GB/128GB", soluong:1, dongia:4790000, description:"Blue White"}
+let sp44 = {masp:44,hinhanh:'../image/Vivo/Vivo-Y33S.jpg', tensp:"Vivo Y33s", soluong:1, dongia:6690000, description:"Blue Yellow Black"}
+let sp45 = {masp:45,hinhanh:'../image/Vivo/Vivo-Y21s-6GB.jpg', tensp:"Vivo Y21s 6GB/128GB", soluong:1, dongia:5290000, description:"Blue White"}
+let sp46 = {masp:46,hinhanh:'../image/Vivo/vivo-Y15s.jpg', tensp:"Vivo Y15s", soluong:1, dongia:3190000, description:"Blue White"}
+let sp47 = {masp:47,hinhanh:'../image/Vivo/Vivo-X70-Pro.jpg', tensp:"Vivo X70 Pro", soluong:1, dongia:16490000, description:"Blue Black"}
+let sp48 = {masp:48,hinhanh:'../image/Vivo/Vivo-V20.jpg', tensp:"Vivo V20", soluong:1, dongia:8490000, description:"Blue Black"}
+let sp49 = {masp:49,hinhanh:'../image/Vivo/Vivo-V20-SE-Bla-1.jpg', tensp:"Vivo V20 SE", soluong:1, dongia:7190000, description:"White Blue Black"}
+let sp50 = {masp:50,hinhanh:'../image/Vivo/VV-Y20-2021.jpg', tensp:"Vivo Y20 2021", soluong:1, dongia:3990000, description:"Blue Black"}
+
+let dssp = [sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10,sp11,sp12,sp13,sp14,sp15,sp16,sp17,sp18,sp19,sp20,sp21,sp22,sp23,sp24,sp25,sp26,sp27,sp28,sp29,sp30,sp31,sp32,sp33,sp34,sp35,sp36,sp37,sp38,sp39,sp40,sp41,sp42,sp43,sp44,sp45,sp46,sp47,sp48,sp49,sp50]
+localStorage.setItem("products", JSON.stringify(dssp))
+let dssp_vv = [sp41,sp42,sp43,sp44,sp45,sp46,sp47,sp48,sp49,sp50]
+localStorage.setItem("products_vv", JSON.stringify(dssp_vv))
+
+let tk1={matk:1,user:"tranthiendat",pass:"tranthiendat123"}
+let tk2={matk:2,user:"nhanviena",pass:"nhanviena123"}
+let tk3={matk:3,user:"nhanvienb",pass:"nhanvienb123"}
+
+
+
+let dstk=[tk1,tk2,tk3]
+localStorage.setItem("taiKhoan", JSON.stringify(dstk))
+
